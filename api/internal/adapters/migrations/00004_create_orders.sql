86f164513ccd58455ec7ff5cea1449f98012e7f9
FSO_0 <bildadsimiyu6@gmail.com>
2026-06-18 14:46:58 +0300

    feat: Create orders migration

---
+++ api/internal/adapters/migrations/00004_create_orders.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+CREATE TABLE orders(
+    order_id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    user_id BIGINT NOT NULL,
+
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+    created_at DATETIME NOT NULL DEFAUlT NOW()
+);
+
+CREATE TABLE order_items (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    order_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    total_price BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+-- +goose Down
+SELECT 'down SQL query';
