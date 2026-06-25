987f7381b3dd3f147c88aa9ecda9a2daee507a6b
FSO_0 <bildadsimiyu6@gmail.com>
2026-06-26 00:42:22 +0300

    debug: ensure SignedString recieves the byte string secret

--- api/go.mod
+++ api/go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/go-chi/chi/v5 v5.3.0
 	github.com/go-playground/validator/v10 v10.30.3
 	github.com/go-sql-driver/mysql v1.10.0
+	github.com/golang-jwt/jwt/v5 v5.3.1
+	golang.org/x/crypto v0.52.0
 )
 
 require (
@@ -13,9 +15,7 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	golang.org/x/crypto v0.52.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
 )
--- api/internal/auth/auth-utils/token.go
+++ api/internal/auth/auth-utils/token.go
@@ -8,13 +8,16 @@ import (
 	"github.com/rawbil/ecom2/internal/config"
 )
 
-func GenerateAuthToken(secret []byte, user_id int) (string, error) {
+func GenerateAuthToken(secret []byte, userID int) (string, error) {
 	expiration := time.Second * time.Duration(config.GetJwtConfig().JwtExpire)
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
-		"userId":    strconv.Itoa(user_id),
-		"expiresAt": time.Now().Add(expiration).Unix(),
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userID":    strconv.Itoa(userID),
+		"expiredAt": time.Now().Add(expiration).Unix(),
 	})
 
-	return token.SignedString(token)
-
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
