47b5f6cb04059690772223039702bdeb017e610c
FSO_0 <bildadsimiyu6@gmail.com>
2026-06-30 13:04:09 +0300

    refactor: Modify user table with refresh_token_id from the refresh_tokens table

--- api/internal/adapters/migrations/00006_update_user_with_token.sql
+++ api/internal/adapters/migrations/00006_update_user_with_token.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 ALTER TABLE users
 ADD COLUMN refresh_token_id BIGINT,
-ADD CONSTRAINT fk_rt
+ADD CONSTRAINT fk_rt 
 FOREIGN KEY (refresh_token_id) REFERENCES refresh_tokens(id);
 
 -- +goose Down
