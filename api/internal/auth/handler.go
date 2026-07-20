72cf6fb2550eb01da4cc720a5470967a42bdf300
FSO_0 <bildadsimiyu6@gmail.com>
2026-07-20 23:51:31 +0300

    refactor: update email sending(WIP)

--- api/internal/auth/handler.go
+++ api/internal/auth/handler.go
@@ -2,10 +2,14 @@ package auth
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 
 	repository "github.com/rawbil/ecom2/internal/adapters/sqlc"
 	authutils "github.com/rawbil/ecom2/internal/auth/auth-utils"
+	"github.com/rawbil/ecom2/internal/config"
+	"github.com/rawbil/ecom2/internal/email"
 	"github.com/rawbil/ecom2/internal/utils"
 )
 
@@ -47,23 +51,23 @@ func (h *Handler) UserRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//& Email
-	// email_data := RegisterEmailData{
-	// 	Username: registerParams.Username,
-	// }
+	email_data := RegisterEmailData{
+		Username: registerParams.Username,
+	}
 
-	// html, err := email.RenderHtml(os.DirFS("internal/email/templates"), email_data)
-	// if err != nil {
-	// 	utils.ErrorHandler(err, "oops... server error", w, http.StatusInternalServerError)
-	// 	return
-	// }
+	html, err := email.RenderHtml(os.DirFS("internal/email/templates"), email_data)
+	if err != nil {
+		utils.ErrorHandler(err, "oops... server error", w, http.StatusInternalServerError)
+		return
+	}
 
-	// sender := email.NewResendSender(config.GetResendConfig().ApiKey, config.GetResendConfig().EmailFrom)
-	// if err := sender.EmailConfig(registerParams.Email, "Registration Successful", html); err != nil {
-	// 	utils.ErrorHandler(err, "oops... server error", w, http.StatusInternalServerError)
-	// 	return
-	// } else {
-	// 	log.Println("Email Sent Successfully")
-	// }
+	sender := email.NewResendSender(config.GetResendConfig().ApiKey, config.GetResendConfig().EmailFrom)
+	if err := sender.EmailConfig(registerParams.Email, "Registration Successful", html); err != nil {
+		utils.ErrorHandler(err, "oops... server error", w, http.StatusInternalServerError)
+		return
+	} else {
+		log.Println("Email Sent Successfully")
+	}
 
 	utils.JsonResponse(w, utils.SuccessMessage{
 		Message: "Registration success!",
